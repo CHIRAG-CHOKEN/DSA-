0aaa352d0ab6ee289590a08554bf4fee7b75dbfe
CHIRAG-CHOKEN <chokenchirag89@gmail.com>
2025-01-19 01:14:40 +0530

    new

---
+++ cpp/ARRAY PRACTISE/1.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+//hold
+void printArray(int arr[],int size) {
+
+}
+
+int main() {
+    //declare
+    int number[15];
+    
+    //accesing an aray
+    cout<<"Value at 21 index is :" << number[14] << endl;
+
+    //cout<<"value of 20 index"<<number[20]<<endl;
+
+    //initialising an array
+    int second[3]={5,7,1};
+
+    //accesing an element
+    cout<<"Value at 2nd index is :" <<second[2]<<endl;
+
+    //another example
+    int third[15]={2,3};
+    //printing the array
+    int n =15;  
+    printArray(third,15) ;             //size if array
+   
+   //intililising the loaction with 0
+   int fourth[10]={0}
+   
+   // cout<<"printing the array"<<endl;
+    //for(int i=0; i<n;i++){
+    //    cout<<third[i] <<"";
+    }
+
+    cout<<"Everything is fine"<<endl <<endl;
+    return 0;
+
+}
